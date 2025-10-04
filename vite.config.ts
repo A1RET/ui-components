24a74912b7abd6ebf07a35a88678d97141e34ade
Airet <thiefon@gmail.com>
2025-10-04 17:18:56 +0500

    Added config for vite-plugin-svg-spritemap.

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,29 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        vueDevTools(),
+        VitePluginSvgSpritemap('./src/assets/icons/**/*.svg', {
+            styles: false,
+            prefix: false,
+            route: 'sprite',
+            svgo: {
+                plugins: ['preset-default', 'removeDimensions'],
+            },
+            output: {
+                name: 'sprite.svg',
+                view: false,
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
     },
-  },
 })
