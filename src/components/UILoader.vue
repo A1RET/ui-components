a34439bde21ff16a7798833f0e662fdecb7f6677
Airet <thiefon@gmail.com>
2025-10-04 18:32:19 +0500

    Added loader component.

---
+++ src/components/UILoader.vue
@@ -0,0 +1,17 @@
+<template>
+    <span class="loader">
+        <svg
+            viewBox="0 0 24 24"
+            class="loader__icon svg-icon"
+            fill="none"
+            stroke="currentColor"
+            stroke-dasharray="13.333333333333334, 36"
+            stroke-dashoffset="0"
+            stroke-width="1.5"
+            focusable="false"
+            aria-hidden="true"
+        >
+            <circle cx="12" cy="12" r="8"></circle>
+        </svg>
+    </span>
+</template>
