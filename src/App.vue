8b8f14afbc7fd08fe0c3ce07e06da5f4c5b6d5e5
Airet <thiefon@gmail.com>
2025-10-04 18:33:24 +0500

    Added button examples to home page.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
-<script setup lang="ts"></script>
-
 <template>
-    <h1>You did it!</h1>
-    <p>
-        Visit
-        <a href="https://vuejs.org/" target="_blank" rel="noopener"> vuejs.org</a>
-        to read the documentation
-    </p>
+    <div class="main-wrapper">
+        <main class="main">
+            <section class="home container">
+                <div class="home__item">
+                    <h2 class="h2">Buttons</h2>
+                    <div class="home__components">
+                        <UIButton> Button </UIButton>
+                        <UIButton size="l"> Another size </UIButton>
+                        <UIButton color="gray"> Another color </UIButton>
+                        <UIButton clear> Transparent </UIButton>
+                        <UIButton is-loading> With icon </UIButton>
+                        <UIButton disabled> Disabled </UIButton>
+                    </div>
+                </div>
+                <div class="home__item">
+                    <h2 class="h2">Buttons group</h2>
+                    <div class="home__components">
+                        <div class="button-group">
+                            <UIButton> Button </UIButton>
+                            <UIButton size="l"> Another size </UIButton>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
 </template>
+
+<script setup lang="ts">
+import UIButton from '@/components/UIButton.vue'
+</script>
