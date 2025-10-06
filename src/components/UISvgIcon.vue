dbd404a3dae6107910a213fb1f9abe81446eaf63
Airet <thiefon@gmail.com>
2025-10-06 17:10:16 +0500

    Added UISvgIcon component.

---
+++ src/components/UISvgIcon.vue
@@ -0,0 +1,11 @@
+<template>
+    <svg class="svg-icon">
+        <use :xlink:href="`/sprite#${id}`" />
+    </svg>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    id: string;
+}>();
+</script>
